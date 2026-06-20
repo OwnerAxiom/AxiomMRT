3dbecaff5ec752b3b549d2ef9b7703bd9a5aac51
The Maanav <prettylioness0550@gmail.com>
2026-06-20 17:15:03 +0530

    OwnerAxiom

--- Axiom.py
+++ Axiom.py
@@ -3,24 +3,30 @@ from pyrogram import __version__
 from pyrogram.raw.all import layer
 from info import Config
 import logging
-from datetime import datetime
-import logging.config, os
-from pytz import timezone
+import logging.config
 from aiohttp import web
-import pyromod
 
-logging.config.fileConfig('logging.conf')
+logging.config.fileConfig("logging.conf")
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes([web.get("/", lambda r: web.Response(text="𝐀‌xɪσϻ 𝐌‌ᴧss 𝐑‌єᴘσꝛᴛєꝛ 𝐁‌σᴛ ✅"))])
+    web_app.add_routes(
+        [
+            web.get(
+                "/",
+                lambda r: web.Response(
+                    text="𝐀‌xɪσϻ 𝐌‌ᴧss 𝐑‌єᴘσꝛᴛєꝛ 𝐁‌σᴛ ✅"
+                ),
+            )
+        ]
+    )
     return web_app
 
 
-class Bot (Client):
+class Bot(Client):
 
     def __init__(self):
         super().__init__(
@@ -29,24 +35,38 @@ class Bot (Client):
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
-            plugins={'root': 'plugins'}
+            plugins={"root": "plugins"},
         )
 
-    async def start(self):
+    async def start(self, *args, **kwargs):
         await super().start()
+
         me = await self.get_me()
         self.mention = me.mention
         self.username = me.username
+
         app = web.AppRunner(await web_server())
         await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, Config.PORT).start()
-        logging.info(f"✅ {me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}. ✅")
 
+        await web.TCPSite(
+            app,
+            "0.0.0.0",
+            Config.PORT
+        ).start()
 
-        await self.send_message(Config.OWNER, f"**{me.first_name}  ɪs 𝐒‌ᴛᴧꝛᴛєᴅ 𝐒‌υᴄᴄєssғυʟʟʏ ✨️**")
+        logging.info(
+            f"✅ {me.first_name} with Pyrogram v{__version__} "
+            f"(Layer {layer}) started on {me.username}. ✅"
+        )
 
-        
+        try:
+            await self.send_message(
+                Config.OWNER,
+                f"**{me.first_name}  ɪs 𝐒‌ᴛᴧꝛᴛєᴅ 𝐒‌υᴄᴄєssғυʟʟʏ ✨️**"
+            )
+        except Exception as e:
+            logging.error(f"Owner message error: {e}")
+
     async def stop(self, *args):
         await super().stop()
         logging.info("Bot Stopped ⛔")
